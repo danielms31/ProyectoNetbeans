7496121bdca4820897bc2e90aec202b5a4e24e07
Daniel Merino <daniel.merino@iesjulianmarias.es>
2024-04-30 10:28:51 +0200

    Primera version del proyecto

---
+++ src/main/java/com/mycompany/holamundo/Holamundo.java
@@ -0,0 +1,33 @@
+package com.mycompany.holamundo;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Holamundo {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola desde el main");
+        saluda();
+        System.out.println("Hola desde el main 2");
+        System.out.println("Hola desde el main 3");
+        System.out.println("Hola desde el main 4");
+        
+        int a = 8;
+        int b = 9; 
+        int c = 11; 
+        int d = 12;
+        
+        for (int i=0;i<10;i++){
+            System.out.println("i es = "+i);
+        }
+        
+    }
+    
+    public static void saluda(){
+        System.out.println("Hola desde la funcion saluda");
+        System.out.println("Hola desde la funcion saluda 2");
+        System.out.println("Hola desde la funcion saluda 3");
+    }
+    
+}
